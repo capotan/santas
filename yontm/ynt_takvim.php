21295b372b1f6b82c104b806d7c3af90a209c94a
capotan <afakan@yandex.com>
2013-10-31 23:27:40 +0200

    yönetim akademik takvim

--- yontm/ynt_takvim.php
+++ yontm/ynt_takvim.php
@@ -1,1 +1,30 @@
-akademik takvim
+
+<table>
+<tr><td></td><td>Güz Yarıyılı</td><td>Bahar Yarıyılı</td></tr>
+<tr><td>
+Ders Kayıtları : </td><td><ul><li>Başlangıç:<input type="date" name="gkayit1" value=""></li><li>Bitiş:<input type="date" name="gkayit2" value=""></li></ul> </td><td><ul>
+<li>Başlangıç:<input type="date" name="bkayit1" value=""></li><li>Bitiş:<input type="date" name="bkayit2" value=""></li></ul> 
+</td></tr><tr><td>
+Yarıyıl : </td><td><ul><li>Başlangıç:<input type="date" name="gyari1" value=""></li><li>Bitiş:<input type="date" name="gyari2" value=""></li></ul> </td><td>
+<ul><li>Başlangıç:<input type="date" name="byari1" value=""></li><li>Bitiş:<input type="date" name="byari2" value=""></li></ul> 
+</td></tr><tr><td>
+Dersler: </td><td><ul><li>Başlangıç:<input type="date" name="gders1" value=""></li><li>Bitiş:<input type="date" name="gders2" value=""></li></ul> </td><td>
+<ul><li>Başlangıç:<input type="date" name="bders1" value=""></li><li>Bitiş:<input type="date" name="bders2" value=""></li></ul> 
+</td></tr><tr><td>
+Ekle-Sil Günleri:</td><td> <ul><li>Başlangıç:<input type="date" name="gekle1" value=""></li><li>Bitiş:<input type="date" name="gekle2" value=""></li></ul> </td><td>
+	<ul><li>Başlangıç:<input type="date" name="bekle1" value=""></li><li>Bitiş:<input type="date" name="bekle2" value=""></li></ul> 
+</td></tr><tr><td>
+Derslerin Sona Ermesi: </td><td><input type="date" name="gson" value=""></td><td><input type="date" name="bson" value="">
+</td></tr><tr><td>
+Yarıyıl Sonu sınavları:</td><td><ul><li>Başlangıç:<input type="date" name="gsinav1" value=""></li><li>Bitiş:<input type="date" name="gsinav2" value=""></li></ul>  </td><td>
+<ul><li>Başlangıç:<input type="date" name="bsinav1" value=""></li><li>Bitiş:<input type="date" name="bsinav2" value=""></li></ul> 
+</td></tr><tr><td>
+Yarıyıl Sonu Sınavlarının Otomasyon Sistemine Not Girişi için Son Gün </td><td> <input type="date" name="gsoto" value=""></td><td><input type="date" name="bsoto" value="">
+</td></tr><tr><td>
+Bütünleme Sınavları: </td><td><ul><li>Başlangıç:<input type="date" name="gbut1" value=""></li><li>Bitiş:<input type="date" name="gbut2" value=""></li></ul>  </td><td>
+<ul><li>Başlangıç:<input type="date" name="bbut1" value=""></li><li>Bitiş:<input type="date" name="bbut2" value=""></li></ul> 
+</td></tr><tr><td>
+Bütünleme Sınavlarının Otomasyon Sistemine Not Girişi için Son Gün </td><td> <input type="date" name="gboto" value=""></td><td><input type="date" name="bboto" value="">
+</td></tr>
+<tr><td><input type="submit" value="Kaydet"></td></tr>
+</table>
--- yontm/ynt_ynt.php
+++ yontm/ynt_ynt.php
@@ -1,1 +1,93 @@
-yönetim 
+<?php
+include "../database.php";
+function ogretim()
+{
+	?>
+<table>
+<tr><td><a href="ynt_home.php?s=ynt&y=ekle">Ekle</a></td>
+<?php
+
+$a=1;
+$sorgu=mysql_query("SELECT * from ynt_elemanlari");
+while($sonuc=mysql_fetch_array($sorgu))
+{
+echo "<tr>";
+echo "<td>".$sonuc['unvan']."</td>";
+echo "<td>".$sonuc['isim']."</td>";
+echo "<td>".$sonuc['mail']."</td>";
+echo "<td>".$sonuc['telefon']."</td>";
+echo "<td><a href='ynt_home.php?s=ynt&y=duzenle&d=".$sonuc['id']."'>Düzenle</a></td>";
+echo "<td><a href='ynt_home.php?s=ynt&y=sil&d=".$sonuc['id']."'>Sil</a></td>";
+echo "</tr>";
+
+	$a++;
+}
+?></table>
+
+<?php
+}
+function duzenle($id)
+{
+	if ($_POST['isim']) {
+		mysql_query(" UPDATE ynt_elemanlari SET unvan='".$_POST['unvan']."', isim='".$_POST['isim']."', mail='".$_POST['mail']."', telefon='".$_POST['telefon']."'  WHERE id=$id ");
+		header("location:ynt_home.php?s=ynt");
+	}
+	else
+	{
+	$sorgu1=mysql_query("SELECT * from ogr_elemanlari where id=$id");
+	$sonuc1=mysql_fetch_array($sorgu1);
+?>
+	<form method="post" enctype="multipart/form-data">
+
+		<input type="text" name="unvan" value=" <?php echo $sonuc1['unvan']; ?>">
+		<input type="text" name="isim" value=" <?php echo $sonuc1['isim']; ?>">
+		<input type="text" name="mail" value="<?php echo $sonuc1['mail']; ?>">
+		<input type="text" name="telefon" value=" <?php echo $sonuc1['telefon']; ?>">
+		<input type="submit" value="Kaydet">
+
+	</form>
+<?php
+}
+}
+function sil($id)
+{
+	mysql_query("delete  from ynt_elemanlari where id=$id");
+	header("location:ynt_home.php?s=ynt");
+}
+function ekle()
+{
+	if ($_POST['isim']) {
+		mysql_query("insert into  ynt_elemanlari (unvan,isim,mail,telefon) values ('".$_POST['unvan']."','".$_POST['isim']."','".$_POST['mail']."','".$_POST['telefon']."')  ");
+		header("location:ynt_home.php?s=ynt");
+	}
+	else
+	{
+		?>
+	<form method="post" enctype="multipart/form-data">
+		Ünvan:<input type="text" name="unvan" >
+		İsim:<input type="text" name="isim" >
+		E-mail:<input type="text" name="mail" >
+		Telefon:<input type="text" name="telefon" >
+		<input type="submit" value="Kaydet">
+
+	</form>
+<?php
+}
+}
+switch ($_GET['y']) {
+	case 'duzenle':
+		duzenle($_GET['d']);
+		break;
+	case 'sil':
+		sil($_GET['d']);
+		break;
+	case 'ekle':
+		ekle();
+		break;
+	
+	default:
+		ogretim();
+		break;
+}
+
+?>
